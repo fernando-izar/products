873a0a9ca4b1f4ef2e27d8c76db9bcf10b24a80c
Fernando Cristante Izar <fci@Mac-mini-Fernando.local>
2023-10-28 18:03:56 -0300

    feat: promotional_price

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,8 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from product.models import Product
+from product_category.models import ProductCategory
 
 
 class ProductSerializer(ModelSerializer):
+    promotional_price = SerializerMethodField()
+
+    def get_promotional_price(self, obj):
+        discount = ProductCategory.objects.get(id=obj.product_category.id).discount
+        price_after_discount = obj.price - (obj.price * discount / 100)
+        return round(price_after_discount, 2)
+
     class Meta:
         model = Product
         fields = "__all__"
