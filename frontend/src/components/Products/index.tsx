93b151f762e2d31637647855b333737fae589326
Fernando Cristante Izar <fci@Mac-mini-Fernando.local>
2023-10-28 17:10:21 -0300

    feat: useListProducts, api axios, react-query

--- frontend/src/components/Products/index.tsx
+++ frontend/src/components/Products/index.tsx
@@ -1,4 +1,8 @@
+import useListProducts from "../../hooks/useListProducts";
+
 export const Products = () => {
+  const { data } = useListProducts("");
+  console.log(data);
   return (
     <div>
       <h1>Products fka;sdfjasd;kfjasd;fkja</h1>
