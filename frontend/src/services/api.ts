490395cc69be349897264888b10ecb069605544b
Fernando Cristante Izar <fci@Mac-mini-Fernando.local>
2023-10-28 22:09:56 -0300

    fix: env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: apiUrl || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
